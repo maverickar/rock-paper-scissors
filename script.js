01b640ad03b5d6536df81f476b3cd684a48fd3f3
maverickar <83903931+maverickar@users.noreply.github.com>
2023-09-29 11:31:33 +0200

    Add choice buttons, scoreboard, result, 5 rounds game.

--- script.js
+++ script.js
@@ -1,28 +1,56 @@
-function getComputerChoice() {
+const playerScoreElement = document.querySelector("#player-score");
+const computerScoreElement = document.querySelector("#computer-score");
+const buttons = document.querySelectorAll(".button");
+const result = document.querySelector(".result");
+let playerSelection;
+let computerSelection;
+let playerScore = 0;
+let computerScore = 0;
+
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => playerSelection = button.id);
+    button.addEventListener("click", () => playRound(playerSelection));
+})
+
+function getComputerSelection() {
     let options = ["rock", "paper", "scissors"];
     return options[Math.floor(Math.random() * options.length)]; 
 }
 
-function getPlayerChoice() {
-    return prompt("Please select Rock, Paper or Scissors.").toLowerCase();
+function disableButtons() {
+    buttons.forEach(button => {button.disabled = true})
 }
 
-function playRound() {
-    let computerSelection = getComputerChoice();
-    let playerSelection = getPlayerChoice();
+function checkWinner() {
+    if(playerScore === 5) {
+        result.textContent = "Game over. Player wins!"
+        disableButtons();
+    } else if(computerScore === 5) {
+        result.textContent = "Game over. Computer wins!"
+        disableButtons();
+    }
+}
+
+function playRound(playerSelection) {
+    result.textContent = "";
+    computerSelection = getComputerSelection();
     if (playerSelection === computerSelection) {
-        console.log("Shit, the PC and you had the same idea");
+        result.textContent = "It's a tie!";
     } else if ((playerSelection === "rock" && computerSelection === "scissors") || 
                (playerSelection === "scissors" && computerSelection === "paper") ||
                (playerSelection === "paper" && computerSelection === "rock")) {
-                    console.log("You won! The computer chose " + computerSelection);
+                    playerScore++;
+                    playerScoreElement.textContent = `Player: ${playerScore}`;
+                    result.textContent = "You won!";
                  } else {
-                    console.log("You lost. You chose " + playerSelection + " and the computer chose " + computerSelection);
+                    computerScore++;
+                    computerScoreElement.textContent = `Computer: ${computerScore}`;
+                    result.textContent = "You lost :(";
                  }
     
     console.log("player " + playerSelection, "comp " + computerSelection);
+
+    checkWinner();
+
 }
-
-playRound();
-
-
